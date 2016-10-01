0feb12cb00859d2d8b1b0d368c56506b80e7350d
Curtis Hatter <mitchell.hatter@gmail.com>
2016-10-01 13:04:00 -0400

    feat: implement chunk/4

--- src/List/Addendum.elm
+++ src/List/Addendum.elm
@@ -1,4 +1,4 @@
-module List.Addendum exposing (at, fetch)
+module List.Addendum exposing (at, chunk, fetch)
 
 
 at : List a -> Int -> a -> a
@@ -14,6 +14,49 @@ fetch list index =
 
         False ->
             fetch_list list index
+
+
+chunk : List a -> Int -> Maybe Int -> Maybe (List a) -> Result String (List (List a))
+chunk list count step leftover =
+    let
+        step' =
+            Maybe.withDefault count step
+    in
+        case ( count > 0, step' > 0 ) of
+            ( False, _ ) ->
+                Result.Err "Count must be a positive integer greater than 0"
+
+            ( _, False ) ->
+                Result.Err "Step must be a positive integer greater than 0"
+
+            ( True, True ) ->
+                Result.Ok <| chunk' list count step' leftover
+
+
+chunk' : List a -> Int -> Int -> Maybe (List a) -> List (List a)
+chunk' list count step leftover =
+    let
+        t =
+            List.take count list
+    in
+        case list of
+            [] ->
+                []
+
+            val ->
+                if List.length t == count then
+                    t :: chunk' (List.drop step val) count step leftover
+                else
+                    case leftover of
+                        Nothing ->
+                            []
+
+                        Just x ->
+                            (x
+                                |> List.take (1 + step - List.length t)
+                                |> List.append t
+                            )
+                                :: (chunk' [] 0 0 Nothing)
 
 
 fetch_list : List a -> Int -> Maybe a
