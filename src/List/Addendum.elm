a3b3143d32c69aa1f59a8aa1026b04773ef7d63f
Curtis Hatter <mitchell.hatter@gmail.com>
2016-10-01 13:04:50 -0400

    docs: document chunk

--- src/List/Addendum.elm
+++ src/List/Addendum.elm
@@ -1,11 +1,19 @@
 module List.Addendum exposing (at, chunk, fetch)
 
+{-|
 
+@docs at, chunk, fetch
+
+-}
+
+
+{-| -}
 at : List a -> Int -> a -> a
 at list index default =
     Maybe.withDefault default <| fetch list index
 
 
+{-| -}
 fetch : List a -> Int -> Maybe a
 fetch list index =
     case index < 0 of
@@ -16,6 +24,19 @@ fetch list index =
             fetch_list list index
 
 
+{-| Chunks a List into sublists containing `count` elements each, where each new
+    chunk starts `step` elements into the List. `step` is optional and, if not
+    provided, defaults to `count`. If the final chunk does not have `count`
+    elements, and `leftover` is `Nothing`, then the last chunk is discarded;
+    however, if `leftover` is not `Nothing` then elements are taken as necessary
+    to make the last chunk `count` elements long. If there are not enough
+    elements in `leftover` then the chunk is still return with less than `count`
+    elements.
+
+    chunk [1, 2, 3, 4, 5, 6] 2 Nothing Nothing == [[1, 2], [3, 4], [5, 6]]
+    chunk [1, 2, 3, 4, 5, 6] 3 (Just 2) (Just [7, 8]) == [[1, 2, 3], [3, 4, 5], [5, 6, 7]]
+
+-}
 chunk : List a -> Int -> Maybe Int -> Maybe (List a) -> Result String (List (List a))
 chunk list count step leftover =
     let
