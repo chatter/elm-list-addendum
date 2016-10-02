f4729ddc02a953ee729d0ea267ab5f80a6e5c48d
Curtis Hatter <mitchell.hatter@gmail.com>
2016-10-02 03:04:14 -0400

    test: tests for find/3

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -22,6 +22,7 @@ all =
         , drop_while_tests
         , each_tests
         , fetch_tests
+        , find_tests
         ]
 
 
@@ -179,4 +180,21 @@ fetch_tests =
             \() -> Expect.equal Nothing <| fetch [ 1, 3, 5 ] 3
         , test "return Nothings at invalid negative index" <|
             \() -> Expect.equal Nothing <| fetch [ 1, 3, 5 ] -4
+        ]
+
+
+find_tests =
+    describe "List.Addendum.find/3"
+        [ test "returns Nothing when no match found and no default given" <|
+            \() ->
+                find (\a -> a `rem` 2 == 1) Nothing [ 2, 4, 6 ]
+                    |> Expect.equal Nothing
+        , test "returns default when no match found and default given" <|
+            \() ->
+                find (\a -> a `rem` 2 == 1) 0 [ 2, 4, 6 ]
+                    |> Expect.equal 0
+        , test "returns first matching element from list when found" <|
+            \() ->
+                find (\a -> a `rem` 2 == 1) Nothing [ 2, 3, 4, 5 ]
+                    |> Expect.equal 3
         ]
